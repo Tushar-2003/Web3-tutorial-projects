b6f748e192042d7c1e71b4493728189bbbc5bad1
Tushar-2003 <tushargupta3002@gmail.com>
2023-11-27 19:40:39 +0530

    ok?

--- NFT-Hardhat-basic/contracts/NFTee.sol
+++ NFT-Hardhat-basic/contracts/NFTee.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 // Import the openzepplin contracts
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // NFTee is  ERC721 signifies that the contract we are creating imports ERC721 and follows ERC721 contract from openzeppelin
 contract NFTee is ERC721 {
